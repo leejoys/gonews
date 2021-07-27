67c0aefcac275e160e0c6c3a9096fef8ae80c06c
leejoys <user@example.local>
2021-07-27 04:12:36 +0300

    fix

--- pkg/datasource/datasource_test.go
+++ pkg/datasource/datasource_test.go
@@ -13,12 +13,12 @@ func TestSource_Run(t *testing.T) {
 	// Создаем источник данных
 	cByte, err := os.ReadFile("./aconfig.json")
 	if err != nil {
-		t.Fatalf("main ioutil.ReadFile error: %s", err)
+		t.Fatalf("TestSource_Run ioutil.ReadFile error: %s", err)
 	}
 	ds := &Source{}
 	err = json.Unmarshal(cByte, ds)
 	if err != nil {
-		t.Fatalf("main json.Unmarshal error: %s", err)
+		t.Fatalf("TestSource_Run json.Unmarshal error: %s", err)
 	}
 	ds.PostChan = make(chan storage.Post)
 	ds.ErrorChan = make(chan error)
